ca3b72f1bd95f33be4d37f6d4a0fa0283e26e97e
Walter Bormans <walter.bormans@paradoxcat.com>
2021-01-23 14:19:21 +0100

    Adds command line parsing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log("Hello from the app.")
+import arg from "arg"
+import { readFile } from "fs/promises";
+import { exit } from "process";
+
+const args = arg({
+    // Types
+    '--help': Boolean,
+    '--employees': String,
+
+    // Aliases
+    '-e': '--employees',
+    '-h': '--help',
+});
+
+if(args["--help"]){
+    // display help
+    console.log('Usage: call-router [--employees <employee_file>]')
+    exit(0);
+}
+
+const employeeFile = args["--employees"]
+
+console.log(`You want to load employees from '${args["--employees"]}'.`)
+readFile(employeeFile ?? "", 'utf8')
+    .then(JSON.parse)
+    .then(data => console.log(data))
